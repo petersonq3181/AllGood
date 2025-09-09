1221808d0edba4e04ec72ba9048c94dbfc0af310
Quinn Peterson <qap2001@gmail.com>
2025-09-08 17:37:56 -0700

    Initial Commit

---
+++ AllGood/AllGoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllGoodApp.swift
+//  AllGood
+//
+//  Created by Quinn Peterson on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AllGoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllGoodTests/AllGoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllGoodTests.swift
+//  AllGoodTests
+//
+//  Created by Quinn Peterson on 9/8/25.
+//
+
+import Testing
+@testable import AllGood
+
+struct AllGoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
